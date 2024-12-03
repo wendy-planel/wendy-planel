1e59c96118b857cea7ef37951af40d2a1f6bf14c
ylei <2457738122@qq.com>
2024-12-03 16:55:02 +0800

    refactor: 优化功能按钮模块  feat: 对接下载存档按钮

--- src/components/tips/tips.tsx
+++ src/components/tips/tips.tsx
@@ -1,19 +1,19 @@
 import { useState, useCallback } from 'react';
-import './tooltip.css';
+import './tips.css';
 
-interface TooltipProps {
+interface HovertipProps {
   children: React.ReactNode
   tip: string
 }
-export const Tooltip = (props: TooltipProps) => {
+export const Hovertip = (props: HovertipProps) => {
   const { children, tip } = props;
   const [visible, setVisible] = useState(false);
   const showTip = useCallback(() => setVisible(true), []);
   const hideTip = useCallback(() => setVisible(false), []);
   return (
-    <div className="tooltip-container" onMouseEnter={showTip} onMouseLeave={hideTip}>
+    <div className="hovertip-container" onMouseEnter={showTip} onMouseLeave={hideTip}>
       {children}
-      <div className={`tooltip ${visible ? 'visible' : ''}`}>{tip}</div>
+      <div className={`hovertip ${visible ? 'visible' : ''}`}>{tip}</div>
     </div>
   );
 };
